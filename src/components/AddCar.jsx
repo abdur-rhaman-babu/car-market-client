7d0b0eaf4325fd20f86ec61c847f40f5518cb3f1
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-02 18:11:23 +0600

    car-market-client set up

---
+++ src/components/AddCar.jsx
@@ -0,0 +1,68 @@
+const AddCar = () => {
+    const handleAddCar = e =>{
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const price = form.price.value;
+        const brand = form.brand.value;
+        const photo = form.photo.value
+
+        const newCar = {name, price, brand, photo}
+        console.log(newCar)
+    }
+
+  return (
+    <div className="bg-gray-200 mt-10 md:w-1/4 lg:w-2/4 mx-auto p-5">
+      <h1 className="font-bold text-center text-2xl">ADD CAR</h1>
+      <div>
+        <form onSubmit={handleAddCar} className=" my-3">
+          <div className="grid lg:grid-cols-2 gap-5">
+          <div>
+            <label>Name</label>
+            <br />
+            <input
+              className="border outline-none w-full p-2 mt-2"
+              type="text"
+              name="name"
+              placeholder="name"
+            />
+          </div>
+          <div>
+            <label>Price</label>
+            <br />
+            <input
+              className="border outline-none w-full p-2 mt-2"
+              type="text"
+              name="price"
+              placeholder="price"
+            />
+          </div>
+          <div>
+            <label>Brand</label>
+            <br />
+            <input
+              className="border outline-none w-full p-2 mt-2"
+              type="text"
+              name="brand"
+              placeholder="brand"
+            />
+          </div>
+          <div>
+            <label>Photo</label>
+            <br />
+            <input
+              className="border outline-none w-full p-2 mt-2"
+              type="text"
+              name="photo"
+              placeholder="photo"
+            />
+          </div>
+          </div>
+          <input className="w-full bg-black p-2 mt-5 text-white font-bold" type="submit" value="Add Car" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddCar;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Mainlayout from './components/Mainlayout.jsx'
+import Home from './components/Home.jsx'
+import AddCar from './components/AddCar.jsx'
+import UpdateCar from './components/UpdateCar.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Mainlayout/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/addCar',
+        element:<AddCar/>
+      },
+      {
+        path:'/updateCar',
+        element:<UpdateCar/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
